e01a2816a9ae2433860f610f0424c028013b180b
Luca Rager <lucarager@gmail.com>
2024-11-12 10:42:35 -0800

    Init

---
+++ Code/Mod.cs
@@ -0,0 +1,81 @@
+﻿namespace Platter
+{
+    using System.Reflection;
+    using Colossal.IO.AssetDatabase;
+    using Colossal.Logging;
+    using Game;
+    using Game.Input;
+    using Game.Modding;
+
+    public class Mod : IMod
+    {
+        /// <summary>
+        /// The mod's default name.
+        /// </summary>
+        public const string ModName = "Platter";
+
+        /// <summary>
+        /// Gets the active instance reference.
+        /// </summary>
+        public static Mod Instance { get; private set; }
+
+        /// <summary>
+        /// Gets the mod's active settings configuration.
+        /// </summary>
+        internal ModSettings ActiveSettings { get; private set; }
+
+        /// <summary>
+        /// Gets the mod's active log.
+        /// </summary>
+        internal ILog Log { get; private set; }
+
+        // Demo stuff
+        public static ProxyAction m_ButtonAction;
+        public static ProxyAction m_AxisAction;
+        public static ProxyAction m_VectorAction;
+        public const string kButtonActionName = "ButtonBinding";
+        public const string kAxisActionName = "FloatBinding";
+        public const string kVectorActionName = "Vector2Binding";
+
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            // Set instance reference.
+            Instance = this;
+
+            // Initialize logger.
+            Log = LogManager.GetLogger(ModName);
+#if DEBUG
+            Log.Info("setting logging level to Debug");
+            Log.effectivenessLevel = Level.Debug;
+#endif
+            Log.Info($"loading {ModName} version {Assembly.GetExecutingAssembly().GetName().Version}");
+
+            // Apply harmony patches.
+            new Patcher("lucachoo-Platter", Log);
+
+            // Register mod settings to game options UI.
+            ActiveSettings = new ModSettings(this);
+            ActiveSettings.RegisterInOptionsUI();
+
+            // Load saved settings.
+            AssetDatabase.global.LoadSettings("Platter", ActiveSettings, new ModSettings(this));
+        }
+
+        public void OnDispose()
+        {
+            Log.Info("disposing");
+            Instance = null;
+
+            // Clear settings menu entry.
+            if (ActiveSettings != null)
+            {
+                ActiveSettings.UnregisterInOptionsUI();
+                ActiveSettings = null;
+            }
+
+            // Revert harmony patches.
+            Patcher.Instance?.UnPatchAll();
+        }
+    }
+}
---
+++ Code/ModSettings.cs
@@ -0,0 +1,38 @@
+﻿namespace Platter
+{
+    using Colossal.IO.AssetDatabase;
+    using Game;
+    using Game.Modding;
+    using Game.SceneFlow;
+    using Game.Settings;
+
+    /// <summary>
+    /// The mod's settings.
+    /// </summary>
+    [FileLocation(Mod.ModName)]
+    public class ModSettings : ModSetting
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModSettings"/> class.
+        /// </summary>
+        /// <param name="mod"><see cref="IMod"/> instance.</param>
+        public ModSettings(IMod mod)
+            : base(mod)
+        {
+        }
+
+        /// <summary>
+        /// Restores mod settings to default.
+        /// </summary>
+        public override void SetDefaults()
+        {
+        }
+
+        /// <summary>
+        /// Determines whether we're currently in-game (in a city) or not.
+        /// </summary>
+        /// <returns><c>false</c> if we're currently in-game, <c>true</c> otherwise (such as in the main menu or editor).</returns>
+        public bool IsNotInGame() => GameManager.instance.gameMode != GameMode.Game;
+    }
+
+}
